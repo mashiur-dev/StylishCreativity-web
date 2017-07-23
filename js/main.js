27a4e2216cca848b5d50707c07054097f0fb4477
shoyebzz <shoyeb.abir21@gmail.com>
2017-07-24 01:30:21 +0600

    half markup done - 24 july

---
+++ js/main.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+   
+    $(".client_reviews").owlCarousel({
+      loop: true,
+      autoplay: true,
+      items: 1,
+      nav: true,
+      autoplayHoverPause: true,
+      animateOut: 'slideOutUp',
+      animateIn: 'slideInUp'
+    });
+    
+    
+    
+});
